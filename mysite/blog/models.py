9b8782bcba4510bba741591b0d4de75a033d769d
Oleg Urban <urban.oleg1@gmail.com>
2021-01-25 19:42:58 +0300

    Fix some bugs and made migrations

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.utils import timezone
-from djang.contrib.auth.models import User
+from django.contrib.auth.models import User
 
 class Post(models.Model):
     STATUS_CHOICES = (
@@ -8,7 +8,7 @@ class Post(models.Model):
         ('published', 'Published'),
     )
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250, unique_fordate='publish')
+    slug = models.SlugField(max_length=250, unique_for_date='publish')
     author = models.ForeignKey(User, on_delete=models.CASCADE,
                                related_name='blog_posts')
     body = models.TextField()
@@ -18,7 +18,7 @@ class Post(models.Model):
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
 
     class Meta:
-        ordering = ('-publish')
+        ordering = ('-publish',)
 
     def __str__(self):
         return self.title
