165bd28fec7b7a221060245fab66a3ea5101adc1
Oleg Urban <urban.oleg1@gmail.com>
2021-01-26 08:33:49 +0300

    create class handler

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
 from .models import Post
-
+    
 
 def post_list(request):
     object_list = Post.published.all()
@@ -20,3 +21,10 @@ def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post, status='published', publish__year=year,
     publish__month = month, publish__day=day)
     return render(request, 'blog/post/detail.html', {'post': post})
+
+
+class PostListView(ListView):
+    queryset = Post.objects.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html/'
