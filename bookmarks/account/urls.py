b63656b1f319b6df49db44ab8b31eac029c35f59
Oleg Urban <urban.oleg1@gmail.com>
2021-02-12 18:38:35 +0300

    Added views for changing password

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
+from django.urls import reverse_lazy
 from . import views
 
 app_name = 'account'
@@ -9,5 +10,11 @@ urlpatterns = [
     # path('login/', views.user_login, name='login')
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('password_change/',
+          auth_views.PasswordChangeView.as_view(success_url = reverse_lazy('account:password_change_done')),
+          name='password_change'),
+    path('password_change/done',
+          auth_views.PasswordChangeDoneView.as_view(),
+          name="password_change_done"),
     path('', views.dashboard, name='dashboard'),
 ]
