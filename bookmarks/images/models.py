f4c1bf42509b251f57c324f7efb1244b31f0c5e5
Oleg Urban <urban.oleg1@gmail.com>
2021-02-16 13:21:49 +0300

    Created a detail view for images

--- bookmarks/images/models.py
+++ bookmarks/images/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+from django.urls import reverse
 from django.utils.text import slugify
 
 class Image(models.Model):
@@ -23,3 +24,7 @@ class Image(models.Model):
         if not self.slug:
             self.slug = slugify(self.title)
         super().save(*args, **kwargs)
+    
+    def get_absolute_url(self):
+        return reverse('images:detail', args=[self.id, self.slug])
+    
