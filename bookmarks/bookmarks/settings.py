d312c9e14d10b1388e95827f4d8db80dd54bc340
Oleg Urban <urban.oleg1@gmail.com>
2021-02-16 09:44:45 +0300

    Added a image model and made migrations

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'social_django',
     'django_extensions',
-    'images.app.ImagesConfig',
+    'images.apps.ImagesConfig',
 ]
 
 MIDDLEWARE = [
