510eb39f624cd021cf56314ec0e459640b5ccda7
Oleg Urban <urban.oleg1@gmail.com>
2021-02-17 16:43:00 +0300

    Created a AJAX view to follow users

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -34,7 +34,8 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     # edit profile
     path('edit/', views.edit, name='edit'),
-    
+
     path('users/', views.user_list, name='user_list'),
+    path('users/follow/', views.user_follow, name='user_follow'),
     path('users/<username>/', views.user_detail, name='user_detail'),
 ]
