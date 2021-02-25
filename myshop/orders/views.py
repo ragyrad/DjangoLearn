d5e8c6267d85600fc558c61f398594c2943afba9
Oleg <urban.oleg1@gmail.com>
2021-02-25 13:34:48 +0300

    Created asynch email sending about placed orders with celery and rabbitmq

--- myshop/orders/views.py
+++ myshop/orders/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from cart.cart import Cart
+
 from .models import OrderItem
 from .forms import OrderCreateForm
-
+from .tasks import order_created
 
 def order_create(request):
     cart = Cart(request)
@@ -16,6 +17,8 @@ def order_create(request):
                                          price=item['price'],
                                          quantity=item['quantity'])
             cart.clear()
+            # launch asynchronous task
+            order_created.delay(order.id)
             return render(request,
                           'orders/order/created.html',
                           {'order': order})
