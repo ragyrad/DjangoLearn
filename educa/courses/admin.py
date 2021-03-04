2b6f84f9cf66b15653fdda4cc8d194c62042e8eb
Oleg <urban.oleg1@gmail.com>
2021-03-04 16:50:17 +0300

    Added caching

--- educa/courses/admin.py
+++ educa/courses/admin.py
@@ -19,3 +19,7 @@ class CourseAdmin(admin.ModelAdmin):
     list_filter = ['created', 'subject']
     search_fields = ['title', 'overview']
     prepopulated_fields = {'slug': ('title',)}
+
+
+# use memcache admin index site
+admin.site.index_template = 'memcache_status/admin_index.html'
