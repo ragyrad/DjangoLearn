3db261a60587db49f0708a3b0e550b7927fadbe3
Oleg <urban.oleg1@gmail.com>
2021-03-04 09:33:16 +0300

    Created a public area for displaying courses

--- educa/courses/views.py
+++ educa/courses/views.py
@@ -1,14 +1,16 @@
 from django.apps import apps
+from django.db.models import Count
 from django.urls import reverse_lazy
 from django.shortcuts import redirect, get_object_or_404
 from django.forms.models import modelform_factory
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from django.views.generic.base import TemplateResponseMixin, View
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from braces.views import CsrfExemptMixin, JSONRequestResponseMixin
 
-from .models import Course, Module, Content
+from .models import Course, Subject, Module, Content
 from .forms import ModuleFormset
 
 
@@ -144,3 +146,25 @@ class ContentOrderView(CsrfExemptMixin, JSONRequestResponseMixin, View):
         for id, order in self.request_json.items():
             Content.objects.filter(id=id, module__course__owner=request.user).update(order=order)
         return self.render_json_response({'saved': 'OK'})
+
+
+class CourseListView(TemplateResponseMixin, View):
+    model = Course
+    template_name = 'courses/course/list.html'
+
+    def get(self, request, subject=None):
+        subjects = Subject.objects.annotate(total_courses=Count('courses'))
+        courses = Course.objects.annotate(total_modules=Count('modules'))
+
+        if subject:
+            subject = get_object_or_404(Subject, slug=subject)
+            courses = courses.filter(subject=subject)
+
+        return self.render_to_response({'subjects': subjects,
+                                        'subject': subject,
+                                        'courses': courses})
+
+
+class CourseDetailView(DetailView):
+    model = Course
+    template_name = 'courses/course/detail.html'
